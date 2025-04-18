6aaa07d65df79540d6f0af07525e9ee0770be6be
laws65 <seanes118@gmail.com>
2025-04-18 11:36:01 +0930

    auto add singletons

--- cwispy.gd
+++ cwispy.gd
@@ -2,11 +2,31 @@
 extends EditorPlugin
 
 
+const AUTOLOADS = [
+	{name="Multiplayer", path="res://addons/cwispy/singletons/multiplayer.gd"},
+	{name="Clock", path="res://addons/cwispy/singletons/clock.gd"},
+	{name="GameManager", path="res://addons/cwispy/singletons/game_manager.gd"},
+	{name="ServerTicker", path="res://addons/cwispy/singletons/server_ticker.gd"},
+	{name="SnapshotManager", path="res://addons/cwispy/singletons/snapshot_manager.gd"},
+	{name="Synchroniser", path="res://addons/cwispy/singletons/synchroniser.gd"},
+]
+
+
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
-	pass
+	for autoload in AUTOLOADS:
+		add_autoload_singleton(autoload.name, autoload.path)
+	
+	if not ProjectSettings.has_setting("autoload/NetworkedInput"):
+		add_autoload_singleton("NetworkedInput", "res://addons/cwispy/singletons/networked_input.gd")
+
+	var input = ProjectSettings.get_setting("autoload/NetworkedInput")
+	if input == "*res://addons/cwispy/singletons/networked_input.gd":
+		push_error("
+			NetworkedInput autoloads needs to be added by the user, extending _NetworkedInput \n
+			Please delete existing NetworkedInput autoload and add your own
+		")
 
 
 func _exit_tree() -> void:
-	# Clean-up of the plugin goes here.
-	pass
+	for autoload in AUTOLOADS:
+		remove_autoload_singleton(autoload.name)
